9532b5e7cd1489470f82e71a248b9966fdcc5642
mohamedabotir <mohamedabotyear@gmail.com>
2022-07-17 16:42:15 +0200

    fiinsh to batch

--- queries/Chapter10/SQlQuery.sql
+++ queries/Chapter10/SQlQuery.sql
@@ -9,3 +9,16 @@ set @num = (select postalcode from Sales.Customers where custid = 1)
 SELECT @num;
 
 EXEC sp_help 'Sales.Customers';
+
+go;--end of batch
+DECLARE @num as int= 50;
+
+PRINT @num
+
+go;
+IF OBJECT_ID('dbo.T1', 'U') IS NOT NULL DROP TABLE dbo.T1;
+CREATE TABLE dbo.T1(col1 INT IDENTITY);
+SET NOCOUNT ON;
+insert into T1 DEFAULT VALUEs;
+go 100;
+select * from Nums;
